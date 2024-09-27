8cea200ce630102d94cd4dca695c29b6851769d3
lancekakkassery <114832335+lancekakkassery@users.noreply.github.com>
2024-09-27 01:46:42 +0000

    Shifted front end around and added .env again

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return render_template('home.html')
+    return render_template('home2.html')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
